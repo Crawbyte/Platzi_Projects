68b16d7f0aec4b58ec7d0c6c5a7b7927be78d343
Cristian Roso <cristian.roso@gmail.com>
2021-02-07 11:24:21 -0300

    general pairs and correlations

--- R_Fundamentos/statistics.r
+++ R_Fundamentos/statistics.r
@@ -1,1 +1,18 @@
 library(dplyr)
+library(stringr)
+
+# Import document
+orangeec <- read_csv("Orange-Economy-master/orangeec.csv")
+
+# General pairs 
+pairs(orangeec[,2:6])
+pairs(orangeec[,5:10])
+
+# subsetting
+newdata <- subset(orangeec, select=c(5, 6, 10, 11, 12, 13))
+
+# Pairs of the subset
+pairs(newdata)
+
+# Correlation
+cor(orangeec[,2,6], use='complete.obs')
