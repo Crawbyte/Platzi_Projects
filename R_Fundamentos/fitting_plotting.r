14cf43b5b887f687e1eb539632ad33aa8899065b
Cristian Roso <cristian.roso@gmail.com>
2021-02-08 09:22:42 -0300

    End of the lesson

--- R_Fundamentos/fitting_plotting.r
+++ R_Fundamentos/fitting_plotting.r
@@ -2,3 +2,19 @@ library(dplyr)
 
 # Import document
 orangeec <- read_csv("Orange-Economy-master/orangeec.csv")
+
+# Fitting
+orangeec <- orangeec %>%
+  mutate(Crecimiento_GDP = ifelse(`GDP Growth %` >= 2.5, '2.5 o más', 'Menos 2.5%'))
+
+orangeec <- orangeec %>%
+  mutate(Anaranjados= ifelse(`Creat Ind % GDP` >= 2.5, 'Más anaranjados', 'Menos anaranjados'))
+
+# Ranking
+orangeec %>%
+  arrange(desc(`Creat Ind % GDP`))
+
+TopNaranjas <- orangeec %>%
+  filter(Country %in% c('Mexico', 'Panama', 'Argentina', 'Colombia', 'Brazil'))
+
+TopNaranjas %>% arrange(desc(`Creat Ind % GDP`))
