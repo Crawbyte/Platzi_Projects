6cc52e81f2ef1b1b55165ef8b444858b5c030989
Cristian Roso <cristian.roso@gmail.com>
2021-02-08 09:51:00 -0300

    Colors and facewrat

--- R_Fundamentos/fitting_plotting.r
+++ R_Fundamentos/fitting_plotting.r
@@ -1,4 +1,5 @@
 library(dplyr)
+library(RColorBrewer)
 
 # Import document
 orangeec <- read_csv("Orange-Economy-master/orangeec.csv")
@@ -18,3 +19,20 @@ TopNaranjas <- orangeec %>%
   filter(Country %in% c('Mexico', 'Panama', 'Argentina', 'Colombia', 'Brazil'))
 
 TopNaranjas %>% arrange(desc(`Creat Ind % GDP`))
+
+# Facet wrap
+ggplot(TopNaranjas, aes(x=`Internet penetration % population`, 
+                        y=`Services % GDP`, size=`GDP PC`)) + 
+  geom_point() + facet_wrap(~Country)
+
+ggplot(TopNaranjas, aes(x=`Education invest % GDP`, 
+                        y=`Creat Ind % GDP`, size=Unemployment)) + 
+  geom_point() + facet_wrap(~Country)
+
+my_colors <- brewer.pal(9, 'Reds')
+
+ggplot(TopNaranjas, aes(x=`Internet penetration % population`,
+                        y=`GDP PC`, fill=`Creat Ind % GDP`))+
+  geom_tile() +
+  facet_wrap(~Country) +
+  scale_fill_gradientn(colors = my_colors)
