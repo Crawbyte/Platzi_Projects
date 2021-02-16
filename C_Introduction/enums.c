c8d87c1870465167a3ed6e5a3a7aedd2c0df32b0
crawbyte <cristian.roso@gmail.com>
2021-02-16 16:27:15 -0300

    card deck example

--- C_Introduction/enums.c
+++ C_Introduction/enums.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
-enum weekDays {Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
+enum deck 
+{
+    club = 0,
+    diamonds = 5,
+    hearts = 10,
+    spades = 15,
+} card;
 
 int main()
 {
-    enum weekDays today;
-    today = Wednesday;
-        printf("Day %d", today+1);
+    card = spades;
+    printf("Card power %d", sizeof(card));
         return 0;
 }
