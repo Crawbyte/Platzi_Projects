601b93bdf83af1362868dba18ad0d269fc530e34
crawbyte <cristian.roso@gmail.com>
2021-02-16 17:36:35 -0300

    variables examples

---
+++ C_Introduction/variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Variable declarations
+
+extern int a, b, c;
+
+float f, g, h;
+
+int main()
+{
+    // Variable definition
+    int a, b, c;
+    // Variable initialization
+    a = 1;
+    b = 34;
+    c = a + b;
+    printf("Value of sum is: %d", c);
+    return 0;
+}
