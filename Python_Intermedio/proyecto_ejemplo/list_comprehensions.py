fe615cb8c3e0de6d877788e719cf594543115f9f
crawbyte <cristian.roso@gmail.com>
2021-04-09 10:24:47 -0400

    lists and dict

--- Python_Intermedio/proyecto_ejemplo/list_comprehensions.py
+++ Python_Intermedio/proyecto_ejemplo/list_comprehensions.py
@@ -4,9 +4,9 @@ def run():
     #     if i % 3 != 0:
     #         squares.append(i**2)
 
-    squares = [i**2 for i in range(1, 101) if i % 3 != 0]
-
-    print(squares)    
+    # squares = [i**2 for i in range(1, 101) if i % 3 != 0]
+    list_comprehension = [i for i in range(1,100000) if i % 4 == 0 and i % 6 == 0 and i % 9 == 0]
+    print(list_comprehension)    
 
 
 if __name__ == '__main__':
